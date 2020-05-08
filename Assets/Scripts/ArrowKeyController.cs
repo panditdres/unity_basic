4d3032fa340b4f27b9f2fb969e86fe46d0a7cf3c
Pandit Adhilaga-Dres <panditadhilagadres@pandits-mbp.lan>
2020-05-08 19:06:48 +0100

    init commit

---
+++ Assets/Scripts/ArrowKeyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowKeyController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+    // need to change the look of the cursor as it goes over different assets
+    // need to tell the interaction with objects
+    // potentially make the char move to that object 
+    public LayerMask clickableLayer;
+
+    // variables for the different pointers
+    public Texture2D normalPointer; // the normal pointer
+    public Texture2D target; // pointer for clickable objects
+    public Texture2D vehicles; // pointer for clickable vehicles
+    public Texture2D interact; // pointer for interacting with char
+    public Texture2D doorway; // pointer for interacting with door
+
+    public EventVector3 OnClickEnvironment;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            // this is to set the cursor to a clickable object
+            if (hit.collider.gameObject.tag == "Vehicle")
+            {
+                Cursor.SetCursor(vehicles, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if (hit.collider.gameObject.tag == "Object")
+            {
+                Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if (hit.collider.gameObject.tag == "Character")
+            {
+                Cursor.SetCursor(interact, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if(hit.collider.gameObject.tag == "Door")
+            {
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClickEnvironment.Invoke(hit.point);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(normalPointer, Vector2.zero, CursorMode.Auto);
+        }
+    }
+
+}
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3>
+{
+
+}
